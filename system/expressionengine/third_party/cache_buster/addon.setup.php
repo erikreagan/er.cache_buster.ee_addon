481e400c939e457d1037ef75fecb2638cb6a54ed
Jack McNicol <jack@mcpickle.com.au>
2016-08-10 10:05:43 +0930

    Add addon setup file for ee3

---
+++ system/expressionengine/third_party/cache_buster/addon.setup.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'author'      => 'Erik Reagan',
+    'author_url'  => 'http://erikreagan.com',
+    'name'        => 'Cache Buster',
+    'description' => 'Adds a simple cache buster to your flat file references',
+    'version'     => '1.1.1',
+    'namespace'   => '\\',
+);
